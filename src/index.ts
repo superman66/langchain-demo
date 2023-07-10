f0ec13545a52afb941d1c734038cf5eac879db86
superman <supermanchc@gmail.com>
2023-07-10 18:07:11 +0800

    chore: update Azure OpenAI config

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,30 @@
 import * as dotenv from "dotenv";
-import { OpenAI } from "langchain";
+import { LLMChain } from "langchain/chains";
+import { OpenAI } from "langchain/llms/openai";
+import { PromptTemplate } from "langchain/prompts";
 
 dotenv.config();
 
 const model = new OpenAI({
-  modelName: "gpt-3.5-turbo",
-  openAIApiKey: process.env.OPENAI_API_KEY,
+  azureOpenAIApiKey: process.env.AZURE_OPENAI_API_KEY,
+  azureOpenAIApiInstanceName: process.env.AZURE_OPENAI_API_INSTANCE_NAME,
+  azureOpenAIApiDeploymentName: process.env.AZURE_OPENAI_API_DEPLOYMENT_NAME,
+  azureOpenAIApiVersion: process.env.AZURE_OPENAI_API_VERSION,
+  azureOpenAIApiCompletionsDeploymentName:
+    process.env.AZURE_OPENAI_API_COMPLETIONS_DEPLOYMENT_NAME,
+  azureOpenAIBasePath: process.env.AZURE_OPENAI_BASE_PATH,
 });
 
-const res = await model.call(
-  "What's a good idea for an application to build with GPT-3?"
-);
+const template =
+  "你是一个翻译引擎，我会提供你中文，你将其翻译成英文。并要求翻译的内容得体、通顺、符合语法，而且在保证中文意思的前提下，尽量减少英文的字数。下面是需要翻译的中文：{text}";
+
+const prompt = new PromptTemplate({
+  template,
+  inputVariables: ["text"],
+});
+
+const chain = new LLMChain({ llm: model, prompt });
+
+const res = await chain.call({ text: "计算失败，请稍后重试。" });
 
 console.log(res);
